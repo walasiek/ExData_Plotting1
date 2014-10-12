03436722bd1e01b3fe0dee14a309d9f60bb4a217
Marcin Walas <mwalas@amu.edu.pl>
2014-10-13 01:16:53 +0200

    plot3

--- plot3.R
+++ plot3.R
@@ -1,23 +1,53 @@
 # ---------------------------
-# Plot 2
+# Plot 3
 # Author: Marcin Walas
 # ---------------------------
 
-source('readData.R')
+source('readDataDebug.R')
 
 plotType = 's'
 
 plot(
     data$DatePOSIXCol,
     data$Sub_metering_1,
+    xlab = "",
+    ylab = "Energy sub metering",
+    ylim=range(data$Sub_metering_1),
     type=plotType)
 
+par(new=TRUE)
+
+plot(
+    data$DatePOSIXCol,
+    data$Sub_metering_2,
+    xlab="",
+    ylab="",
+    xlim=range(data$DatePOSIXCol),
+    ylim=range(data$Sub_metering_1),
+    col="red",
+    type=plotType)
+
+par(new=TRUE)
+
+plot(
+    data$DatePOSIXCol,
+    data$Sub_metering_3,
+    xlab="",
+    ylab="",
+    xlim=range(data$DatePOSIXCol),
+    ylim=range(data$Sub_metering_1),
+    col="blue",
+    type=plotType)
+
+
 legend(
-    "topright", pch=1,
-    col = c("red","blue"),
+    "topright", pch=NA,
+    col = c("black","red","blue"),
     legend = c('Sub_metering_1',
         'Sub_metering_2',
-        'Sub_metering_3')
+        'Sub_metering_3'),
+    lwd = 2, cex = 1.2,
+    lty = c(1, 1, 1)
     )
 
 dev.copy(png, file='plot3.png')
